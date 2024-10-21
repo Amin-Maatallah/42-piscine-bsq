8d38c0ae8edddc05a176e451342e7535180a3916
Amin <aminmaatallah4@gmail.com>
2024-10-21 20:25:44 +0200

    test_square done

--- srcs/test_square.c
+++ srcs/test_square.c
@@ -6,7 +6,7 @@
 /*   By: amaatall <amaatall@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 17:49:43 by amaatall          #+#    #+#             */
-/*   Updated: 2024/10/21 19:20:35 by amaatall         ###   ########.fr       */
+/*   Updated: 2024/10/21 20:23:50 by amaatall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ int	test_square(int **grid, int x, int y, int size)
 	d = grid[x + size][y + size];
 	if (d - c - b + a > 0)
 	{
-		return (1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 
 t_square	test_squares(int **grid, int rows, int cols)
@@ -40,26 +40,42 @@ t_square	test_squares(int **grid, int rows, int cols)
 	int y;
 
 	size = 2;
-	while (x <= rows && y <= cols)
+
+	x = 0;
+	y = 0;
+
+	square.x = 0;
+	square.y = 0;
+	square.size = 0;
+
+	while (1)
 	{
-		x = 0;
-		while (x + size <= rows)
+		if (test_square(grid, x, y, size))
 		{
-			y = 0;
-			while (y + size <= cols)
+			if (size > square.size)
 			{
-				if (test_square(grid, x, y, size))
-				{
-					square.x = x;
-					square.y = y;
-					square.size = size;
-					return (square);
-				}
+				square.x = x;
+				square.y = y;
+				square.size = size;
+			}
+			size++;
+		}
+		else
+		{
+			if (y + size < cols)
+			{
 				y++;
 			}
-			x++;
+			else if (x + size < rows)
+			{
+				x++;
+				y = 0;
+			}
+			else
+			{
+				break ;
+			}
 		}
-		size--;
 	}
 	return (square);
 };
