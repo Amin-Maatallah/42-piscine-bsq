44f70b69cdc319d8876bef77978f36e3973b90dd
Luke Willis <lwillis@c2r8s3.42warsaw.pl>
2024-10-21 19:37:30 +0200

    Updated bsq.h

--- includes/bsq.h
+++ includes/bsq.h
@@ -6,7 +6,7 @@
 /*   By: amaatall <amaatall@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 10:12:58 by lwillis           #+#    #+#             */
-/*   Updated: 2024/10/21 19:26:55 by amaatall         ###   ########.fr       */
+/*   Updated: 2024/10/21 19:37:10 by lwillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,14 @@ typedef struct s_solution
 }			t_solution;
 
 int			ft_atoi(char *str);
-int			read_map(char *filename);
+t_map		read_map_file(char *filename);
+t_map		make_map(char *map_str);
 int			is_valid_map(char *map_str);
 char		*ft_strdup(char *str, int to);
 int			ft_strlen(char *str);
 int			solve_map(t_map map);
 void		print_grid(char **grid, int rows, int cols);
-char		**get_obstacles_grid(t_map map);
+int			**get_obstacles_grid(t_map map);
 t_map		mock_map(void);
 
 #endif
