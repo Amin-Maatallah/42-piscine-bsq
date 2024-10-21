e2062d084346e12cfc0572b83b49220ea4bcbc8e
Luke Willis <lwillis@c2r8s3.42warsaw.pl>
2024-10-21 20:03:06 +0200

    updated main.c/maps

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,45 @@
 /*   By: amaatall <amaatall@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 09:56:38 by lwillis           #+#    #+#             */
-/*   Updated: 2024/10/21 19:27:09 by amaatall         ###   ########.fr       */
+/*   Updated: 2024/10/21 19:58:01 by lwillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+void	free_all(t_map map)
+{
+	int	i;
+
+	if (map.cols > 0)
+	{
+		i = 0;
+		while (i < map.rows)
+		{
+			free(map.grid[i]);
+			i++;
+		}
+		free(map.grid);
+	}
+}
 
 int	main(int argc, char *argv[])
 {
+	t_map	map;
+
 	if (1 == argc)
-		solve_map(mock_map());
+	{
+		//TODO Replace with stdin map
+		map = mock_map();
+		solve_map(map);
+	}
 	else if (2 == argc)
-		solve_map(read_map_file(argv[1]));
-	return (argc - argc);
+	{
+		map = read_map_file(argv[1]);
+		solve_map(map);
+		free_all(map);
+	}
+	return (0);
 }
