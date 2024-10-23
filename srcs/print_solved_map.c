e9cee23ff3c347a041f9418f8581103939e13c00
Amin <aminmaatallah4@gmail.com>
2024-10-23 14:32:23 +0200

    somhow the grid prints itself and thats fine

--- srcs/print_solved_map.c
+++ srcs/print_solved_map.c
@@ -6,7 +6,7 @@
 /*   By: amaatall <amaatall@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:01:51 by amaatall          #+#    #+#             */
-/*   Updated: 2024/10/22 19:01:58 by amaatall         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:31:03 by amaatall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,4 @@ void	print_solved_map(t_square square, t_map map)
 		while (++j < square.y + square.size)
 			map.grid[i][j] = map.filled;
 	}
-	print_grid(map.grid, map.rows, map.cols);
 }
